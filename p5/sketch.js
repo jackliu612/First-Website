a22b4f5c440f9174233d23ac8da992849be7d167
Jack Liu <jackliu612@gmail.com>
2019-03-15 22:22:01 -0400

    Re-centering

--- p5/sketch.js
+++ p5/sketch.js
@@ -22,14 +22,15 @@ function draw() {
 	line(0, height/2, width, height/2);
 
 
-	var start = (width/2)-(floor(dim/2)*(2*size+padding));
+	var rStart = (width/2)-(floor(dim/2)*(2*size+padding));
+	var cStart = (height/2)-(floor(dim/2)*(2*size+padding));
 	noStroke();
 	fill(100);
 	//"Nice" dark blue color
 	//fill(22, 111, 255);
 	for(var r = 0; r < dim; r++){
 		for(var c = 0; c < dim; c++){
-			rect(start+(2*size+padding)*r, start+(2*size+padding)*c , size, size);
+			rect(rStart+(2*size+padding)*r, cStart+(2*size+padding)*c , size, size);
 		}
 	}
 }
