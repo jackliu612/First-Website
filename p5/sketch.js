b3c05d2c6a67e00fe54e99f315dab238dc53a784
Jack Liu <jackliu612@gmail.com>
2019-03-17 12:03:56 -0400

    Added mouse detectiona and variable grid size

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,19 +1,16 @@
-const dim = 5
-const size = 25;
+const xDim = 5; //Math.floor(Math.random() * 5 +3);
+const yDim = 3; //Math.floor(Math.random() * 5 +3);
+const size = 50;
 const padding = 10;
+
+var rStart;
+var cStart;
+var grid = [yDim];
 
 function setup() {
 
 	var cnv = createCanvas(windowWidth-20, windowHeight-20);
-	background(255, 0, 200);
 	rectMode(RADIUS);
-	/*
-	let myCanvas = createCanvas(1000, 1000);
-	myCanvas.parent('canvas');
-	*/
-}
-
-function draw() {
 
 	//Drawing bounding box
 	line(0,0, width, height);
@@ -21,16 +18,63 @@ function draw() {
 	line(width/2, 0, width/2, height);
 	line(0, height/2, width, height/2);
 
+	makeGrid();
+	drawGrid();
 
-	var rStart = (width/2)-(floor(dim/2)*(2*size+padding));
-	var cStart = (height/2)-(floor(dim/2)*(2*size+padding));
-	noStroke();
-	fill(100);
-	//"Nice" dark blue color
-	//fill(22, 111, 255);
-	for(var r = 0; r < dim; r++){
-		for(var c = 0; c < dim; c++){
-			rect(rStart+(2*size+padding)*r, cStart+(2*size+padding)*c , size, size);
+	/*
+	let myCanvas = createCanvas(1000, 1000);
+	myCanvas.parent('canvas');
+	*/
+}
+
+function draw(){
+	mouseCheck();
+}
+
+function makeGrid(){
+	for(var r = 0; r < yDim; r++){
+		grid[r] = [xDim];
+		for(var c = 0; c < xDim; c++){
+			grid[r][c] = false;
 		}
 	}
+}
+
+function drawGrid(){
+
+	//Works for odd lenghts
+	if(xDim%2!=0){
+		cStart = (width/2)-(floor(xDim/2)*(2*size+padding));		
+	} else{
+		cStart = (width/2)-((xDim-1)/2*(2*size+padding));	
+	}
+
+	if(yDim%2!=0){
+		rStart = (height/2)-(floor(yDim/2)*(2*size+padding));
+	} else{
+		rStart = (height/2)-((yDim-1)/2*(2*size+padding));
+	}
+	//noStroke();
+	//fill(100);
+	//"Nice" dark blue color
+	//fill(22, 111, 255);
+	for(var c = 0; c < xDim; c++){
+		for(var r = 0; r < yDim; r++){
+			rect(cStart+(2*size+padding)*c, rStart+(2*size+padding)*r , size, size);
+		}
+	}
+}
+
+function mouseCheck(){
+	fill(100);
+	for(var c = 0; c < xDim; c++){
+		for(var r = 0; r < yDim; r++){	
+			if(mouseIsPressed && abs(mouseX-(cStart+(2*size+padding)*c))<size && abs(mouseY-(rStart+(2*size+padding)*r))<size){
+				rect(cStart+(2*size+padding)*c, rStart+(2*size+padding)*r , size, size);				
+				grid[r][c] = true;
+				console.log(grid);
+			}
+		}
+	}
+
 }
